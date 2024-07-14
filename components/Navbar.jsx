72975a198d77fc6d88b33032fc2b32d2d1879eeb
Sudip Acharya <drxking456@gmail.com>
2024-07-14 19:41:30 +0545

    nsdlf

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import React from "react";
-
+import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-      <nav className="flex items-center justify-between  px-20 py-5 border-b border-gray-700">
-      <Link href="/" >NEXT.js</Link>
+      <nav className="flex items-center justify-between  px-20 py-5 ">
+      <Link href="/" ><Image src="/next.svg" width="100" height="0" className="invert"></Image></Link>
         <ul className="flex gap-10">
           <Link href="/" >Home</Link>
-          <li>Contact</li>
-          <li>Services</li>
+          <Link href="/contact" >Contact</Link>
+          <Link href="/services" >Services</Link>
           <Link href="/about">About</Link>
         </ul>
       </nav>
