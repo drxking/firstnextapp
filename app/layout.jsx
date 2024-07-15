ea4c382ca6fabfad1d32dcc592546b26e62d4ef8
Sudip Acharya <drxking456@gmail.com>
2024-07-15 14:10:19 +0545

    Commited

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,10 +13,12 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css" />
       <body className={inter.className}>
-          <Navbar />
-          {children}
-          <Footer />
+          
+            <Navbar />
+              {children}
+            <Footer />
       </body>
     </html>
   );
